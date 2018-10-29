9b4b63ca2f6fe7f3ba8138e5e01b10e07f728846
dimati9 <dimati9@mail.ru>
2018-10-29 18:47:59 +0500

    _

---
+++ js/js.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    console.log('ready');
+    $(".menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+    
+     $('#up').click(function() {  
+    $('body,html').animate({scrollTop:0},500);  
+    return false;  
+  });
+    
+  
+    
+    
+    $('#foto').on('click', function(event) {
+         event.preventDefault();
+  $('.mask').addClass('active');
+    $('.foto').addClass('active');
+});
+
+$('.mask').on('click', function() {
+  $('.mask').removeClass('active');
+    $('.foto').removeClass('active');
+});
+});
