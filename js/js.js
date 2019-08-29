1478b6915144c3d68ccd77d6cb8b0854b37f2205
dimati9 <dimati9@yandex.ru>
2019-08-29 10:42:10 +0500

    update 09.2019

--- js/js.js
+++ js/js.js
@@ -11,12 +11,23 @@ $(document).ready(function(){
     $('body,html').animate({scrollTop:0},500);  
     return false;  
   });
-    
-  
-    
+
+    $(document).ready(function() {
+        $('.gallery').magnificPopup({
+            type: 'image',
+            gallery:{
+                enabled:true
+            },
+            delegate: 'a'
+
+        });
+    });
+
     
     $('#foto').on('click', function(event) {
-         event.preventDefault();
+        event.preventDefault();
+
+
   $('.mask').addClass('active');
     $('.foto').addClass('active');
 });
