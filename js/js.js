3dc6f84a0f8c8b9e3e5c1970b832e412ddc9c7a5
dimati9 <dimati9@yandex.ru>
2019-10-28 12:29:20 +0500

    fix

--- js/js.js
+++ js/js.js
@@ -1,39 +1,37 @@
-$(document).ready(function(){
-    console.log('ready');
-    $(".menu").on("click","a", function (event) {
+$(document).ready(function () {
+
+    $(".menu").on("click", "a", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
-        top = $(id).offset().top;
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
-    
-     $('#up').click(function() {  
-    $('body,html').animate({scrollTop:0},500);  
-    return false;  
-  });
 
-    $(document).ready(function() {
-        $('.gallery').magnificPopup({
-            type: 'image',
-            gallery:{
-                enabled:true
-            },
-            delegate: 'a'
-
-        });
+    $('#up').click(function () {
+        $('body,html').animate({scrollTop: 0}, 500);
+        return false;
     });
 
-    
-    $('#foto').on('click', function(event) {
+    $('.gallery').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        delegate: 'a'
+
+    });
+
+    $('#foto').on('click', function (event) {
         event.preventDefault();
 
 
-  $('.mask').addClass('active');
-    $('.foto').addClass('active');
-});
+        $('.mask').addClass('active');
+        $('.foto').addClass('active');
+    });
 
-$('.mask').on('click', function() {
-  $('.mask').removeClass('active');
-    $('.foto').removeClass('active');
-});
+    $('.mask').on('click', function () {
+        $('.mask').removeClass('active');
+        $('.foto').removeClass('active');
+    });
+
 });
